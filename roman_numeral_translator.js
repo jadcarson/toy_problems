5027160a1ec3f4bd00edd4d500eb8695468d6d3c
Jad Carson <Jad.Carson@gmail.com>
2016-09-15 16:40:59 -0500

    Roman Numeral Translator code

---
+++ roman_numeral_translator.js
@@ -0,0 +1,27 @@
+function translateRomanNumeral (romanNumeral) {
+  var totalValue = 0; 
+  var DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+}
+if(romanNumeral.length === 0)
+  return 0;
+
+for(var i = 0; i<romanNumeral.length-1;i++){
+  if(!DIGIT_VALUES[romanNumeral[i]]){
+    return 'null'
+  }
+  if(DIGIT_VALUES[romanNumeral[i]] >= DIGIT_VALUES[romanNumeral[i+1]]){
+    totalValue = totalValue + DIGIT_VALUES[romanNumeral[i]]
+  }else{
+    totalValue = totalValue - DIGIT_VALUES[romanNumeral[i]]
+  }
+}
+totalValue = totalValue + DIGIT_VALUES[romanNumeral[i]]
+return totalValue;
+}
