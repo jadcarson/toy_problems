3bf4fc1ecf9353572748550708d1807ecf24ec2b
Jad Carson <Jad.Carson@gmail.com>
2016-09-19 12:55:46 -0500

    Solution for nested word count

---
+++ problems/nested_word_count.js
@@ -0,0 +1,24 @@
+function nestedWordCount(wordList) {
+  var bigWord = {};
+  var amount = 0;
+  var theWord = '';
+  for (var i = 0; i < wordList.length; i++) {
+    bigWord[wordList[i]] = 0;
+  }
+  for (var key in bigWord) {
+    for (var key2 in bigWord) {
+      if (key.indexOf(key2) !== -1)
+        bigWord[key]++
+    }
+
+  }
+  for (var key3 in bigWord) {
+    console.log(bigWord[key3])
+    if (bigWord[key3] > amount) {
+      amount = bigWord[key3]
+      theWord = key3
+    }
+
+  }
+  return theWord
+}
