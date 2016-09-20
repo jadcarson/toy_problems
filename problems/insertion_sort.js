b40488fa01e40d17728ad7daedb255ec99a84a87
Jad Carson <Jad.Carson@gmail.com>
2016-09-20 14:44:50 -0500

    insertion sort added:

---
+++ problems/insertion_sort.js
@@ -0,0 +1,18 @@
+function insertionSort (array) {
+  for(var i = 1;i<array.length;i++){
+    if(array[i].value<array[i-1].value){
+      var flag = true
+      var mover = array[i];
+      for(var j = i; j>=0; j--){
+        if(j>0 && mover.value<array[j-1].value){
+        array[j] = array[j-1]
+        }
+        else if(flag == true){
+          flag = false;
+          array[j] = mover;
+        }
+      }
+    }
+  }
+  return array;
+}
