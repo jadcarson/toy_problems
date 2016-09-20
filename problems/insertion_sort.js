5fc1f280107cba3bf7fc45334395faf7de904520
Jad Carson <Jad.Carson@gmail.com>
2016-09-20 15:04:56 -0500

    added description and some code clean up

--- problems/insertion_sort.js
+++ problems/insertion_sort.js
@@ -1,13 +1,15 @@
-function insertionSort (array) {
-  for(var i = 1;i<array.length;i++){
-    if(array[i].value<array[i-1].value){
+// Insertion Sort is a basic sorting algorithm that sorts by making a sorted array via taking each value one at a time and moving it to it's relative sorted position
+// withing everything that's been relatively sorted thus far.
+
+function insertionSort(array) {
+  for (var i = 1; i < array.length; i++) {
+    if (array[i].value < array[i - 1].value) {
       var flag = true
       var mover = array[i];
-      for(var j = i; j>=0; j--){
-        if(j>0 && mover.value<array[j-1].value){
-        array[j] = array[j-1]
-        }
-        else if(flag == true){
+      for (var j = i; j >= 0; j--) {
+        if (j > 0 && mover.value < array[j - 1].value) {
+          array[j] = array[j - 1]
+        } else if (flag == true) {
           flag = false;
           array[j] = mover;
         }
@@ -17,4 +19,6 @@ function insertionSort (array) {
   return array;
 }
 
-module.exports = {insertionSort}
+module.exports = {
+  insertionSort
+}
