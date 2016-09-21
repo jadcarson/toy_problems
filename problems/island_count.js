1ba966a6f4b3c7d68b0b659cfbe327364cecadc0
Jad Carson <Jad.Carson@gmail.com>
2016-09-21 14:06:20 -0500

    Island count problem solved

---
+++ problems/island_count.js
@@ -0,0 +1,49 @@
+function countIslands(mapStr) {
+  var eachRow = mapStr.split('\n')
+  var totalArray = [];
+  for (var k = 0; k < eachRow.length; k++) {
+    totalArray.push(eachRow[k].split(''))
+  }
+  var islandObject = []
+  onIsland = false;
+  var number = 0;
+  for (var i = 0; i < totalArray.length; i++) {
+    for (var j = 0; j < totalArray[i].length; j++) {
+      if (totalArray[i][j] == 0) {
+        number++;
+        rIslands(totalArray, i, j)
+      }
+    }
+  }
+
+  function rIslands(map, i, j) {
+    if (i > 0) {
+      if (map[i - 1][j] == 0) {
+        totalArray[i - 1][j] = 1;
+        rIslands(totalArray, i - 1, j)
+      }
+    }
+    if (i < totalArray.length - 1) {
+      if (map[i + 1][j] == 0) {
+        totalArray[i + 1][j] = 1;
+        console.log("Break")
+        rIslands(totalArray, i + 1, j)
+      }
+    }
+    if (j > 0) {
+      if (map[i][j - 1] == 0) {
+        totalArray[i][j - 1] = 1;
+        rIslands(totalArray, i, j - 1)
+      }
+    }
+    if (j < totalArray[0].length - 1) {
+      if (map[i][j + 1] == 0) {
+        totalArray[i][j + 1] = 1;
+        rIslands(totalArray, i, j + 1)
+      }
+
+    }
+  }
+  return number
+}
+module.exports = {countIslands}
