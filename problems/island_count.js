aefa963a67574755f1737a9b8d2234e4056813c6
Jad Carson <Jad.Carson@gmail.com>
2016-09-21 14:22:04 -0500

    added description of problem

--- problems/island_count.js
+++ problems/island_count.js
@@ -1,3 +1,7 @@
+//Given a string representation of a 2d map, return the number of islands in the map. 
+//Land spaces are denoted by a zero, while water is denoted by a dot. 
+//Two land spaces are considered connected if they are adjacent (but not diagonal).
+
 function countIslands(mapStr) {
   var eachRow = mapStr.split('\n')
   var totalArray = [];
